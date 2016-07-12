a062677e693acf9cb33bd3687a2d85ea9504367b
Zhiwei YAN <jerod.yan@gmail.com>
2016-07-12 16:53:39 +0800

    add:udp api

---
+++ api/log_vgtp_pe_udp_api.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# encoding=utf-8
+
+"""
+The Web Server that displays and log the performance parameters
+of VGTP (Vaxum Graphic/Image Transfer Protocol)
+
+"""
+__author__ = "Zhiwei Yan"
+__copyright__ = "Copyright 2016, The VGTP Project"
+__credits__ = ["Zhiwei Yan"]
+__license__ = "Private License"
+__version__ = "1.0"
+__maintainer__ = "Zhiwei YAN"
+__email__ = "jerod.yan@gmail.com"
+__status__ = "Production"
+
+import socket
+import redis
+
+UDP_IP = "172.16.187.231"
+UDP_PORT = 17012
+data_len = 4096
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
+print "Server IP:Port", UDP_IP, UDP_PORT
+
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+while True:
+    data, addr = sock.recvfrom(data_len)
+    print "received msg:", data
+    r.rpush('PE_STAT_LIST', data)
+    
--- webpy/vgtp_pe_main.py
+++ webpy/vgtp_pe_main.py
@@ -21,18 +21,52 @@ import operator
 import os
 import sys
 import re
+import log_config
 
-other_libs={'redis':redis, 'sys':sys, 'operator':operator, 're':re , 'sorted':sorted}
+logger = log_config.log_config("./log/vgtp_pe_main_log.txt", "VGTP_PE")
+logger.info("VGTP_PE start.")
+
+other_libs={'redis':redis, 
+            'sys':sys, 
+            'operator':operator, 
+            're':re , 
+            'sorted':sorted}
+
 render = web.template.render('templates/', globals=other_libs)
 
 urls = (
-    '/', 'index'
+    '/', 'index',
+    '/refresh_data', 'refresh_data'
 )
 
 class index:
     def GET(self):
-        return render.reg_module("reg_module" , "TESTING")
-       
+        r = redis.StrictRedis(host='localhost', port=6379, db=0)
+        content = r.lrange('PE_STAT_LIST', 0, -1)
+
+        if len(content)==0:
+            logger.info("No Data in Redis DB.")
+            return "<html><body><h2> No Data in Redis DB. Wait a min.</h2>\
+                    </body></html>"
+
+        else:        
+            return render.reg_module("reg_module" , "TESTING")
+
+class refresh_data:
+    def GET(self):
+        r = redis.StrictRedis(host='localhost', port=6379, db=0)
+        if r.exists('PE_STAT_LIST'):
+            row = r.lpop('PE_STAT_LIST')
+            if None != row:
+                row = re.sub(r"\s+", "", row)
+                row = re.sub(r"\|$", "", row)
+                logger.info(row)
+                return row
+            else:
+                return "{DATA:NULL}"
+        else:
+            return "{DATA:NULL}"
+        
 if __name__ == "__main__":
     app = web.application(urls, globals())
     app.run()
